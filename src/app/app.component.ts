6074976f1cfc35ad6a52975b571df4a10901847e
Furát Ismail <furat.ismail@cadcom.cz>
2025-05-20 21:49:48 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'example-signal-store';
+  holes = [1, 2, 3, 4, 5]
+
+}
